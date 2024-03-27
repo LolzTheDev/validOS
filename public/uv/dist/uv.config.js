a125b29c4351f96854b96f775fff364e2f493a1b
LolzTheDev <contact@zirc.lol>
2024-03-27 18:23:39 -0500

    UV 2.0.0 Files

--- public/uv/dist/uv.config.js
+++ public/uv/dist/uv.config.js
@@ -1,6 +1,6 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/service/',
+    prefix: '/uv/service/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
