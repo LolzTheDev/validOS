f3068ce05a906f0024f92e0a2671024d0acfc019
LolzTheDev <contact@zirc.lol>
2024-03-27 18:09:53 -0500

    Replace UV files

--- public/uv/dist/uv.config.js
+++ public/uv/dist/uv.config.js
@@ -1,12 +1,12 @@
-// This file overwrites the stock UV config.js
-
+/*global Ultraviolet*/
 self.__uv$config = {
-  prefix: "/uv/service/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+    prefix: '/service/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    client: '/uv.client.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
 };
