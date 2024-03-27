551af1237b76d414518b8eb53574d0d8cd6c9557
LolzTheDev <contact@zirc.lol>
2024-03-27 16:41:55 -0500

    Splash transition, Attempt to fix UV

--- public/uv/dist/uv.config.js
+++ public/uv/dist/uv.config.js
@@ -2,12 +2,11 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
-  bundle: "/uv/uv.bundle.js",
   client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",
   sw: "/uv/uv.sw.js",
 };
