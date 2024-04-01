f03966bd0e0d18db7ae50a9d2f46fd345387900f
LolzTheDev <contact@zirc.lol>
2024-03-31 23:47:40 -0500

    The great Typescriptifying
    (Preparing for future changes)

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { createBareServer } from "@tomphttp/bare-server-node";
 import express from "express";
-import { createServer } from "node:http";
+import { createServer } from "http";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { join } from "node:path";
 import { hostname } from "node:os";
@@ -12,13 +12,9 @@ const publicPath = path.resolve(__dirname, './public');
 const bare = createBareServer("/bare/");
 const app = express();
 
-// Load our publicPath first and prioritize it over UV.
-app.use(express.static(publicPath));
-// Load vendor files last.
-// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
+app.use(express.static(publicPath)); // prioritize our public path
 app.use("/uv/dist/", express.static(uvPath));
 
-// Error for everything else
 app.use((req, res) => {
   res.status(404);
   res.sendFile(join(publicPath, "404.html"));
@@ -47,10 +43,8 @@ let port = parseInt(process.env.PORT || "");
 if (isNaN(port)) port = 8080;
 
 server.on("listening", () => {
-  const address = server.address();
+  const address: any = server.address();
 
-  // by default we are listening on 0.0.0.0 (every interface)
-  // we just need to list a few
   console.log("Listening on:");
   console.log(`\thttp://localhost:${address.port}`);
   console.log(`\thttp://${hostname()}:${address.port}`);
