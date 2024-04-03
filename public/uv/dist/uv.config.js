1656d068a484c49350f5d1088321923ca736bad8
Anshnk <73756572+anshnk@users.noreply.github.com>
2024-04-03 09:57:19 -0500

    Updating UV files to 3.0.0

--- public/uv/dist/uv.config.js
+++ public/uv/dist/uv.config.js
@@ -1,11 +1,12 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/uv/service/',
+    prefix: '/service/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/dist//uv.handler.js',
-    client: '/uv/dist/uv.client.js',
-    bundle: '/uv/dist/uv.bundle.js',
-    config: '/uv/dist/uv.config.js',
-    sw: '/uv/dist/uv.sw.js',
+    handler: '/uv.handler.js',
+    client: '/uv.client.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
 };
